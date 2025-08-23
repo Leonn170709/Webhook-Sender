b5b027b319b319d9c7d60dbd308d5c817aabc80e
Leonn170709 <leon.heberlein@sgahlen.eu>
2025-08-24 01:50:33 +0200

    Update Webhook_Sender.bat

--- Webhook_Sender.bat
+++ Webhook_Sender.bat
@@ -1,69 +1,49 @@
 @echo off
-title Recon Sender
-chcp 65001 >nul
-color 0A
-
-:: ASCII Banner
+:banner
+cls
 echo.
-echo   __        __   _     _                 _
-echo    \ \      / /__^| ^|__ ^| ^|__   ___   ___ ^| ^| _____ _ __   __ _ _ __ ___
-echo     \ \ /\ / / _ \ '_ \^| '_ \ / _ \ / _ \^| ^|/ / __^| '_ \ / _` ^| '_ ` _ \
-echo      \ V  V /  __/ ^|_) ^| ^| ^| ^| (_) ^| (_) ^|   <\__ \ ^|_) ^| (_^| ^| ^| ^| ^| ^| ^|
-echo       \_/\_/ \___^|_.__/^|_^| ^|_^|\___/ \___/^|_^|\_\___/ .__/ \__,_^|_^| ^|_^| ^|_^|
-echo                                                   ^|_^|
+echo """""""""""""""""""""""""""""""""""""""""""""""
+echo " __        __   _     _                 _    "
+echo " \ \      / /__| |__ | |__   ___   ___ | | __"
+echo "  \ \ /\ / / _ \ '_ \| '_ \ / _ \ / _ \| |/ /"
+echo "   \ V  V /  __/ |_) | | | | (_) | (_) |   < "
+echo "  __\_/\_/ \___|_.__/|_| |_|\___/ \___/|_|\_\"
+echo " / ___|  ___ _ __   __| | ___ _ __           "
+echo " \___ \ / _ \ '_ \ / _` |/ _ \ '__|          "
+echo "  ___) |  __/ | | | (_| |  __/ |             "
+echo " |____/ \___|_| |_|\__,_|\___|_|             "
+echo """""""""""""""""""""""""""""""""""""""""""""""
 echo.
-echo ================================================
-echo            Welcome to Webhook Sender 🚀
-echo ================================================
+echo  -----------------------------------------------------------------------------------------
+echo                           Simple Webhook Sender
+echo  -----------------------------------------------------------------------------------------
 echo.
 
-:: Ask for multiple webhooks
-set "webhooks="
-set /p multi="Do you want to enter multiple webhooks? (y/n): "
 
-if /i "%multi%"=="y" (
-    echo Enter your webhooks (one per line).
-    echo Type DONE when finished:
-    :gethooks
-    set /p hook=Webhook:
-    if /i "%hook%"=="done" goto afterhooks
-    if "%hook%"=="" goto afterhooks
-    set "webhooks=!webhooks! %hook%"
-    goto gethooks
-) else (
-    set /p hook=Webhook:
-    set "webhooks=%hook%"
-)
+::  User Input
 
-:afterhooks
-:: Ask for message
-set /p message=Message:
+set /p webhook=Webhook URL : 
+set /p message=Message     : 
+set /p count=Repeat times  : 
+set /p delay=Delay (sec)   : 
 
-:: Ask for how many times
-set /p count=How often to send:
-
-:: Ask for delay
-set /p delay=Delay in seconds:
-
+cls
 echo.
-echo Starting to send messages...
-timeout /t 1 >nul
+echo  Sending "%message%" %count% times with %delay% seconds delay...
+echo  -----------------------------------------------------------------------------------------
+echo.
 
-:: Enable delayed expansion for loop vars
-setlocal enabledelayedexpansion
+::  Sending Loop
 
 for /l %%i in (1,1,%count%) do (
-    cls
-    echo --------------------------------------------------
-    echo Sending message %%i of %count%
-    echo --------------------------------------------------
-    for %%w in (%webhooks%) do (
-        curl -s -X POST -H "Content-Type: application/json" -d "{\"content\": \"%message%\"}" "%%w" >nul
+    echo  Sending message %%i of %count%...
+    curl -s -X POST -H "Content-type: application/json" --data "{\"content\": \"%message%\"}" %webhook%
+    if %%i lss %count% (
+        timeout /t %delay% >nul
     )
-    echo [✓] Message sent!
-    timeout /t %delay% >nul
 )
 
 echo.
-echo All messages sent successfully! 🎉
+echo  All %count% messages have been sent successfully.
+echo.
 pause
